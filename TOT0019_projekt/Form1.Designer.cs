2ac69e764369f87434e67c3abf3969d2b43a9691
Daniel Toth <toth.daniel1999@outlook.com>
2021-04-18 13:14:56 +0200

    ahoj piprs

---
+++ TOT0019_projekt/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace TOT0019_projekt
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.button_add = new System.Windows.Forms.Button();
+			this.button_update = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.button_find = new System.Windows.Forms.Button();
+			this.button_delete = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(12, 13);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.Size = new System.Drawing.Size(567, 379);
+			this.dataGridView1.TabIndex = 0;
+			// 
+			// button_add
+			// 
+			this.button_add.Location = new System.Drawing.Point(165, 496);
+			this.button_add.Name = "button_add";
+			this.button_add.Size = new System.Drawing.Size(99, 23);
+			this.button_add.TabIndex = 1;
+			this.button_add.Text = "Pridat";
+			this.button_add.UseVisualStyleBackColor = true;
+			// 
+			// button_update
+			// 
+			this.button_update.Location = new System.Drawing.Point(375, 495);
+			this.button_update.Name = "button_update";
+			this.button_update.Size = new System.Drawing.Size(99, 23);
+			this.button_update.TabIndex = 2;
+			this.button_update.Text = "Zmenit";
+			this.button_update.UseVisualStyleBackColor = true;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(12, 420);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(118, 20);
+			this.textBox1.TabIndex = 3;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(165, 420);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(148, 20);
+			this.textBox2.TabIndex = 4;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(165, 459);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(76, 20);
+			this.textBox3.TabIndex = 5;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(12, 459);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(118, 20);
+			this.textBox4.TabIndex = 6;
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(336, 459);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(100, 20);
+			this.textBox5.TabIndex = 7;
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(12, 498);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(118, 20);
+			this.textBox6.TabIndex = 8;
+			// 
+			// textBox7
+			// 
+			this.textBox7.Location = new System.Drawing.Point(336, 420);
+			this.textBox7.Name = "textBox7";
+			this.textBox7.Size = new System.Drawing.Size(100, 20);
+			this.textBox7.TabIndex = 9;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(9, 404);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(38, 13);
+			this.label1.TabIndex = 10;
+			this.label1.Text = "Jmeno";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(9, 443);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(43, 13);
+			this.label2.TabIndex = 11;
+			this.label2.Text = "Prijmeni";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(162, 404);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(40, 13);
+			this.label3.TabIndex = 12;
+			this.label3.Text = "Adresa";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(333, 404);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(36, 13);
+			this.label4.TabIndex = 13;
+			this.label4.Text = "Mesto";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(162, 443);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(28, 13);
+			this.label5.TabIndex = 14;
+			this.label5.Text = "PSC";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(9, 482);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(64, 13);
+			this.label6.TabIndex = 15;
+			this.label6.Text = "Rodne Cislo";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(333, 443);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(79, 13);
+			this.label7.TabIndex = 16;
+			this.label7.Text = "Mesto narozeni";
+			// 
+			// button_find
+			// 
+			this.button_find.Location = new System.Drawing.Point(270, 496);
+			this.button_find.Name = "button_find";
+			this.button_find.Size = new System.Drawing.Size(99, 23);
+			this.button_find.TabIndex = 17;
+			this.button_find.Text = "Najit";
+			this.button_find.UseVisualStyleBackColor = true;
+			// 
+			// button_delete
+			// 
+			this.button_delete.Location = new System.Drawing.Point(480, 495);
+			this.button_delete.Name = "button_delete";
+			this.button_delete.Size = new System.Drawing.Size(99, 23);
+			this.button_delete.TabIndex = 18;
+			this.button_delete.Text = "Smazat";
+			this.button_delete.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(590, 527);
+			this.Controls.Add(this.button_delete);
+			this.Controls.Add(this.button_find);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox7);
+			this.Controls.Add(this.textBox6);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button_update);
+			this.Controls.Add(this.button_add);
+			this.Controls.Add(this.dataGridView1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Button button_add;
+		private System.Windows.Forms.Button button_update;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.TextBox textBox7;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Button button_find;
+		private System.Windows.Forms.Button button_delete;
+	}
+}
+
---
+++ TOT0019_projekt/People.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOT0019_projekt
+{
+	class People
+	{
+		public int ID;
+		public string Namee;
+		public string surname;
+		public string Birth_date;
+		public string Adress;
+		public string city;
+		public string Postcode;
+		public string Personal_identification_number;
+		public string City_born;
+
+	}
+}
